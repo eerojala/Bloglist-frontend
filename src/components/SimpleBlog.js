456704ea1a19a347e2b41a897bdbb1f6e2186791
eerojala <eero.ojala@helsinki.fi>
2018-02-20 19:50:04 +0200

    5.12 done

--- src/components/SimpleBlog.js
+++ src/components/SimpleBlog.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const SimpleBlog = ({ blog, onClick }) => (
-  <div>
-    <div>
+  <div className="wrapper">
+    <div className="header">
       {blog.title} {blog.author}
     </div>
-    <div>
+    <div className="likes">
       blog has {blog.likes} likes
       <button onClick={onClick}>like</button>
     </div>
--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -4,6 +4,18 @@ import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
     it('renders content', () => {
-        
+        const blog = {
+            title: 'Component testing with jest and enzyme',
+            author: 'Matti Luukkainen',
+            likes: 7
+        }
+
+        const simpleBlogComponent = shallow(<SimpleBlog blog={blog} />)
+        const headerDiv = simpleBlogComponent.find('.header')
+        const likesDiv = simpleBlogComponent.find('.likes')
+
+        expect(headerDiv.text()).toContain(blog.title)
+        expect(headerDiv.text()).toContain(blog.author)
+        expect(likesDiv.text()).toContain(blog.likes)
     })
 })
