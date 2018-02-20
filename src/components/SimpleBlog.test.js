98aee838c723567ac3fe67ec235917c219e70aa2
eerojala <eero.ojala@helsinki.fi>
2018-02-20 20:05:57 +0200

    5.13 done

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
-    it('renders content', () => {
-        const blog = {
-            title: 'Component testing with jest and enzyme',
-            author: 'Matti Luukkainen',
-            likes: 7
-        }
+    const blog = {
+        title: 'Component testing with jest and enzyme',
+        author: 'Matti Luukkainen',
+        likes: 7
+    }
 
+    it('renders content', () => {
         const simpleBlogComponent = shallow(<SimpleBlog blog={blog} />)
         const headerDiv = simpleBlogComponent.find('.header')
         const likesDiv = simpleBlogComponent.find('.likes')
@@ -17,5 +17,22 @@ describe.only('<SimpleBlog />', () => {
         expect(headerDiv.text()).toContain(blog.title)
         expect(headerDiv.text()).toContain(blog.author)
         expect(likesDiv.text()).toContain(blog.likes)
+    })
+
+    it('event handler is called twice if the like button is pressed twice', () => {
+        const mockHandler = jest.fn()
+
+        const simpleBlogComponent = shallow(
+            <SimpleBlog 
+                blog={blog}
+                onClick={mockHandler}
+            />
+        )
+
+        const button = simpleBlogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+
+        expect(mockHandler.mock.calls.length).toBe(2)
     })
 })
